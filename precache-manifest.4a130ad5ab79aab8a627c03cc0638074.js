c39d7dbeccacf18327ee6845552327fc582b3ed7
Ben Calvert <benhcalvert@gmail.com>
2020-03-11 13:20:06 -0600

    Updates

---
+++ precache-manifest.4a130ad5ab79aab8a627c03cc0638074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38c853698d46f553e33b",
+    "url": "/ReactPortfolio/static/css/main.91089c98.chunk.css"
+  },
+  {
+    "revision": "38c853698d46f553e33b",
+    "url": "/ReactPortfolio/static/js/main.38c85369.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/ReactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "8817bbdb9ad007fac449a330ce43fd6b",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
