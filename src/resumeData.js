3fff98264e72754ca93853949cf129549b209b1c
Ben Calvert <benhcalvert@gmail.com>
2020-03-15 10:35:38 -0600

    added portfolio images

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,122 +1,132 @@
 let resumeData = {
-    "imagebaseurl":"https://github.com/BenHCalvert",
-    "name": "Ben Calvert",
-    "role": "full stack web developer and middle school assistant principal",
-    "linkedinId":"https://www.linkedin.com/in/benjamin-calvert-62978438/",
-    "roleDescription": "",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"linkedin.com/in/benjamin-calvert-62978438/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/BenHCalvert",
-          "className":"fa fa-github"
-        }
-      ],
-    "aboutme":"I recently completed University of Denver's Full Stack Web Development Coding Bootcamp. I also work as a middle school assistant principal. This summer, I'm moving to Boise, ID and am currently in search of a job to leverage and continue developing my technical skills.",
-    "address":"Golden CO | Boise ID",
-    "website":"https://github.com/BenHCalvert",
-    "education":[
-      {
-        "UniversityName":"University of Denver",
-        "specialization":"Coding Bootcamp, Full-Stack Web Development",
-        "MonthOfPassing":"Mar",
-        "YearOfPassing":"2020"
-      },
-      {
-        "UniversityName":"New York University",
-        "specialization":"Bachelor of Arts: Economics, Minor: Italian",
-        "MonthOfPassing":"May",
-        "YearOfPassing":"2013"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"STRIVE Prep - Sunnyside",
-        "specialization":"Assistant Principal",
-        "MonthOfLeaving":"Jun",
-        "YearOfLeaving":"2020",
-        "MonthofStarting":"Jul",
-        "YearOfStarting":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Deer Hill Expeditions",
-        "specialization":"Basecamp Manager",
-        "MonthOfLeaving":"Jul",
-        "YearOfLeaving":"2018",
-        "MonthofStarting":"Jul",
-        "YearOfStarting":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"KIPP New Jersey",
-        "specialization":"School Start-up Project Manager, Head of Student Activities & Operations Manager",
-        "MonthOfLeaving":"Nov",
-        "YearOfLeaving":"2017",
-        "MonthofStarting":"Feb",
-        "YearOfStarting":"2014",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Ben Calvert's technical skills",
-    "skills":[
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Express.JS"
-      },
-      {
-        "skillname":"Git"
-      },
-      {
-        "skillname":"Github"
-      },
-      {
-        "skillname":"HTML"
-      },
-      {
-        "skillname":"JavaScript"
-      },
-      {
-        "skillname":"MongoDB"
-      },
-      {
-        "skillname":"MySql"
-      },
-      {
-        "skillname":"Node.js"
-      },
-      {
-        "skillname":"React"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"Habit App",
-        "description":"An app to help users build habits by tracking their progress towards rewards.",
-        "imgurl":"../public/images/portfolio/coffee.jpg."
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://github.com/BenHCalvert",
+  "name": "Ben Calvert",
+  "role": "full stack web developer and middle school assistant principal",
+  "linkedinId": "https://www.linkedin.com/in/benjamin-calvert-62978438/",
+  "roleDescription": "",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "linkedin.com/in/benjamin-calvert-62978438/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/BenHCalvert",
+      "className": "fa fa-github"
+    }
+  ],
+  "aboutme": "I recently completed University of Denver's Full Stack Web Development Coding Bootcamp. I also work as a middle school assistant principal. This summer, I'm moving to Boise, ID and am currently in search of a job to leverage and continue developing my technical skills.",
+  "address": "Golden CO | Boise ID",
+  "website": "https://github.com/BenHCalvert",
+  "education": [
+    {
+      "UniversityName": "University of Denver",
+      "specialization": "Coding Bootcamp, Full-Stack Web Development",
+      "MonthOfPassing": "Mar",
+      "YearOfPassing": "2020"
+    },
+    {
+      "UniversityName": "New York University",
+      "specialization": "Bachelor of Arts: Economics, Minor: Italian",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2013"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "STRIVE Prep - Sunnyside",
+      "specialization": "Assistant Principal",
+      "MonthOfLeaving": "Jun",
+      "YearOfLeaving": "2020",
+      "MonthofStarting": "Jul",
+      "YearOfStarting": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Deer Hill Expeditions",
+      "specialization": "Basecamp Manager",
+      "MonthOfLeaving": "Jul",
+      "YearOfLeaving": "2018",
+      "MonthofStarting": "Jul",
+      "YearOfStarting": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "KIPP New Jersey",
+      "specialization": "School Start-up Project Manager, Head of Student Activities & Operations Manager",
+      "MonthOfLeaving": "Nov",
+      "YearOfLeaving": "2017",
+      "MonthofStarting": "Feb",
+      "YearOfStarting": "2014",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Ben Calvert's technical skills",
+  "skills": [
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Express.JS"
+    },
+    {
+      "skillname": "Git"
+    },
+    {
+      "skillname": "Github"
+    },
+    {
+      "skillname": "HTML"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "MySql"
+    },
+    {
+      "skillname": "Node.js"
+    },
+    {
+      "skillname": "React"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Habit App",
+      "description": "An app to help users build habits by tracking their progress towards rewards. Built with React.",
+      "projurl": "https://habit-app-2020.herokuapp.com/",
+      "imgurl": "../../public/images/portfolio/starChart.png"
+    },
+    {
+      "name": "Sports Manager",
+      "description": "An app to manage schools sports teams, their rosters and schedules.",
+      "projurl": "https://pacific-peak-82509.herokuapp.com/",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "Hike & Dinner",
+      "description": "An app that allows users to find a nearby hike and a place to grab a bite to eat close to the trailhead",
+      "projurl": "https://benhcalvert.github.io/Group-Project/",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "Weather Dashboard",
+      "description": "A weather dashboard written in HTML and JavaScrpit",
+      "projurl": "https://benhcalvert.github.io/HW6_Weather/",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "PWA Budget Tracker",
+      "description": "A progressive web app budget tracker with offline functionality.",
+      "projurl": "https://aqueous-island-57111.herokuapp.com/",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ]
+}
+
+export default resumeData
